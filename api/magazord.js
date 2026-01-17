5c1c9d9ee7fdb99e4a4f38c3b3d847c2bd31be8a
pablo9hierro <pablo9hierro@gmail.com>
2026-01-17 00:45:19 -0300

    CORS definitivo - Allow-Headers: *

--- api/magazord.js
+++ api/magazord.js
@@ -4,14 +4,17 @@
  */
 
 export default async function handler(req, res) {
-  // Configurar CORS
+  // Configurar CORS COMPLETO
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Max-Age', '86400');
   
   // Responder preflight OPTIONS
   if (req.method === 'OPTIONS') {
-    return res.status(200).end();
+    res.status(200).end();
+    return;
   }
 
   // Permitir apenas POST
